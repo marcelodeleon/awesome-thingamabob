8512e7288ea682a055c9e4e0160c56515075d69c
Marcelo de León <mdeleoncosentino@gmail.com>
2018-10-27 17:07:26 -0300

    setup script WIP

---
+++ ansible-log.md
@@ -0,0 +1,18 @@
+# Mac setup
+
+Before running the ansible playbook, make sure that you have git installed.
+Run ```git --version``` to check. If git is missing, Xcode will prompt an installation.
+
+
+- Ansible Installation
+  - Bootstrap installation with Bash script
+- Homebrew Playbook
+  - Install and update homebrew
+  - Install cask
+  - Neovim cask
+
+
+
+- After main installation is ready
+  - Version Vimwiki and clone it into new mac
+- Check iCloud price to backup photos
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Bootrapping of dev-machine (Awesome Thingamabob) configuration using ANSIBLE.
+# Before running ansible the script will install pip and Homebrew.
+
+function uninstall {
+
+echo "WARNING MARCELO!! This will revert your Awesome Thingamabob setup!"
+echo -n "Are you sure you want to risk it? [y/n] : "
+read confirmation
+
+if [ $confirmation == "y" ]; then
+    echo "Risking it"
+    exit 0
+else
+  echo "Nothing was changed."
+  exit 0
+fi
+
+}
+
+if [ $1 == "uninstall" ]; then
+    uninstall
+fi
+
+echo "================================================="
+echo "Starting the configuration of Awesome Thingamabob"
+echo "================================================="
+
+sudo easy_install pip
+sudo pip install ansible
+
+ansible-playbook -i ./hosts playbook.yml --verbose
+
+echo "Awesome Thingamabob ready for dev!"
+
+exit 0
